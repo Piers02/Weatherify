7ae4b0eb5f9b27f6ebef7d80644e0e773bc62155
Piers02 <piergiorgio.sig@gmail.com>
2023-05-07 15:24:34 +0200

    File fxml

---
+++ src/main/java/com/example/weatherify/WeatherifyController.java
@@ -0,0 +1,32 @@
+package com.example.weatherify;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class WeatherifyController {
+    public TextField tfSearch;
+    public Label avgTemperatureLabel;
+    public Label MaxTemperatureLabel;
+    public Label MinTemperatureLabel;
+    public Label humidityLabel;
+    public Label popLabel;
+    public Label windSpeedLabel;
+
+
+
+    public void onQuitClicked(ActionEvent actionEvent) {
+    }
+
+    public void onAboutClicked(ActionEvent actionEvent) {
+    }
+
+    public void onTemperatureClicked(ActionEvent actionEvent) {
+    }
+
+    public void onPrecipitationClicked(ActionEvent actionEvent) {
+    }
+
+    public void onSearchClicked(ActionEvent actionEvent) {
+    }
+}
